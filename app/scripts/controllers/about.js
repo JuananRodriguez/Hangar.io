f381b21aebfd8f8a3b10e45493fb5349a6897e44
JuananRodriguez <juananrodriguezmartinez@gmail.com>
2018-04-11 12:30:24 +0200

    starting project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hangarApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hangarApp
+ */
+angular.module('hangarApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hangarApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hangarApp
+ */
+angular.module('hangarApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
